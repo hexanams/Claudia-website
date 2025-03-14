5cca3e777cc43ee131a17f981124aab44c92a8cd
Rayamenah <raymondamenah@gmail.com>
2025-03-14 12:12:01 +0100

    changed bg video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -19,7 +19,7 @@ const Hero = () => {
                 preload='auto'
                 className="absolute top-0 left-0 w-full h-full object-cover -z-10"
             >
-                <source src="/hero-video.mp4" type="video/mp4" />
+                <source src="/hero-bg.mp4" type="video/mp4" />
                 Your browser does not support the video tag.
             </video>
             <motion.h1
