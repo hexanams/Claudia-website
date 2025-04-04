198b43840b22edf71532802680f282fa50198ab8
Rayamenah <raymondamenah@gmail.com>
2025-04-04 14:07:38 +0100

    ui fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Home",
+  title: "Empowered therapy - Home page",
   description:
     "Empowered therapy landing page",
   openGraph: {
