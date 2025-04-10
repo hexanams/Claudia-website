ac6d6b8a8b5e1d3e2a6d2608de3609dfa67975e3
Rayamenah <raymondamenah@gmail.com>
2025-04-10 17:06:39 +0100

    fixed favicon

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+// app/head.tsx
+export default function Head() {
+    return (
+        <>
+            <link rel="icon" href="/fav-icon.ico" />
+            <link rel="preload" as="image" href="/booking-hero.svg" />
+            <link rel="preload" as="image" href="/claudia-hero.svg" />
+            <link rel="preload" as="image" href="/payment.svg" />
+            <link rel="preload" as="image" href="/issues.svg" />
+            <link rel="preload" as="image" href="/faq-hero.svg" />
+            <link rel="preload" as="image" href="/payment-1.svg" />
+            <link rel="preload" as="image" href="/payment-2.svg" />
+        </>
+    );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,15 +4,17 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Empowered therapy - Home page",
-  description:
-    "Empowered therapy landing page",
+  description: "Empowered therapy landing page",
+  icons: {
+    icon: "/fav-icon.png",
+  },
   openGraph: {
     images: [{ url: "/fav-icon.png" }],
     title: "Empowered therapy - learn about us",
-    description:
-      "Get to know about the benefits of using empowered therapy",
+    description: "Get to know about the benefits of using empowered therapy",
   },
-}
+};
+
 
 export default function RootLayout({
   children
@@ -22,17 +24,6 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <head>
-        <link rel="icon" href="/fav-icon.png" />
-        <link rel="preload" as="image" href="/booking-hero.svg" />
-        <link rel="preload" as="image" href="/claudia-hero.svg" />
-        <link rel="preload" as="image" href="/payment.svg" />
-        <link rel="preload" as="image" href="/issues.svg" />
-        <link rel="preload" as="image" href="/faq-hero.svg" />
-        <link rel="preload" as="image" href="/payment-1.svg" />
-        <link rel="preload" as="image" href="/payment-2.svg" />
-      </head>
-
       <body className="overflow-x-hidden w-full">
         {/* <Toaster /> */}
         <AppLayout>
